f262931e6dabf28f85c7cbd0439a34137e179f87
Shakilworlds <84674991+Shakilworlds@users.noreply.github.com>
2026-05-17 14:24:17 +0600

    Add main.jsx app entry

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import DigitalAgency from './components/digital-agency/index.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <DigitalAgency />
+  </React.StrictMode>
+)
